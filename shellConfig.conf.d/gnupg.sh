40362bd6e08e83a7ff9d4d6767faf0811c2682c5
Guillaume Bernard <associations@guillaume-bernard.fr>
2018-01-19 15:19:54 +0100

    feat: export des clefs privées PGP

--- shellConfig.conf.d/gnupg.sh
+++ shellConfig.conf.d/gnupg.sh
@@ -20,3 +20,41 @@
 alias gpg="gpg2"
 alias prv="gpg -K"
 alias pub="gpg --list-public-keys"
+
+export_private_keys(){
+    local TEMP_DIR="Clefs privées - $(date +%Y-%m-%d-%H-%M-%S)"
+    local DEST_TAR="${TEMP_DIR}.tar"
+
+    Log ${INFO} "Afin d'exporter les clefs, veuillez vous munir des clefs de déchiffrement de la clé : ${KEY_ID}."
+    Log ${INFO} "Appuyez sur « Entrée » pour continuer."
+    read
+
+    # Preparation de l'environnement
+    mkdir "${TEMP_DIR}"
+
+    # Export des clefs
+    gpg --export-secret-keys --armor "${GPG_MAIN_KEY_ID}" > "${TEMP_DIR}"/GB_FULL_SecretKey_"${GPG_MAIN_KEY_ID}".asc
+    gpg --gen-revoke --armor "${GPG_MAIN_KEY_ID}"  > "${TEMP_DIR}"/GB_REV_"${GPG_MAIN_KEY_ID}".gpg
+
+    # Archivage des clefs
+    tar cvf "${DEST_TAR}" "${TEMP_DIR}"
+
+    Log ${DEBUG} "Suppression des fichiers temporaires..."
+    find "${TEMP_DIR}" -type f -exec shred -u {} \;
+    rmdir "${TEMP_DIR}"
+
+    # Chiffrement de l'archive pour la clef chiffrée
+    gpg2 --output "${DEST_TAR}".gpg --encrypt --recipient "${GPG_MAIN_KEY_ID}" "${DEST_TAR}"
+
+    # Demande de chiffrement pour l'utilisateur
+    Log ${INFO} "Chiffrer également les clefs avec une méthode symétrique ? [O/n]"
+    read reponseQuestion
+    case "${reponseQuestion}" in
+        o|O) Log ${INFO} "Chiffrement de l'archive...\n"
+             gpg2 --output "${DEST_TAR}"symmetric.gpg --symmetric "${DEST_TAR}";;
+        n|N|*) Log ${INFO} "Abandon de la procédure de chiffrement symétrique.";;
+    esac
+
+    Log ${DEBUG} "Suppression de l'archive temporaires..."
+    shred -u "${DEST_TAR}"
+}

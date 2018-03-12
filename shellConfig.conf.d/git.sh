989801710eb0c5f447b7e1ef04674a699baeee61
Guillaume Bernard <associations@guillaume-bernard.fr>
2018-03-12 18:54:13 +0100

    fix: git.sh is moved to external files

--- shellConfig.conf.d/git.sh
+++ shellConfig.conf.d/git.sh
@@ -17,57 +17,36 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-## @var GIT_REPOSITORIES
-## @brief The array that contains repositories information
-declare -a GIT_REPOSITORIES=()
+alias gsb="git status -sb"
+alias gst="git status"
+alias ga="git add"
 
-## @fn __getRepositories()
-## @brief Parse a file to store git repositories
-## @todo What if the key or value contains the char '='
-## With regex : "(.*)"="(.*)"$
-__getRepositories(){
+alias gp="git push"
+alias gl="git pull"
 
-    GIT_REPOSITORIES=()
-    IFS=$'\n'
-    while read -r line;
-    do
-        directoryName=$(cut -d'=' -f1 <<< "${line}")
-        remoteURL=$(cut -d'=' -f2 <<< "${line}")
-        GIT_REPOSITORIES+=("${directoryName}=${remoteURL}")
-    done <<< "$(egrep ".*=.*" < "${GIT_REPOSITORY_FILE}")"
-}
+alias gd="git diff"
+alias gcd="git diff --cached"
+alias gwd="git diff --word-diff"
+alias gwcd="git diff --word-diff --cached"
 
-## @fn gitUpdate()
-## @brief Fetch or clone git repositories
-## @param string --pull if we want to pulling instead of fetching repository
-gitUpdate(){
+alias gco="git checkout"
 
-    local pullOn="${1}"
+alias glg="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(red)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold black)%d%C(reset)' --all"
+alias gslg="lg --show-signature"
 
-    __getRepositories
+alias gbr="git branch"
+alias gbd="git branch -d"
+alias gba="git branch -a"
+alias gbD="git branch -D"
 
-    for line in "${GIT_REPOSITORIES[@]}"; do
-        local directoryName=$(cut -d'=' -f1 <<< "${line}")
-        local remoteURL=$(cut -d'=' -f2 <<< "${line}")
-        local targetDirectory=""
+alias gptag="git push --tags"
+alias gltag="git tag --list -n"
+alias grtags="git tag $2 $1; git push --tags; git tag -d $1; git push origin :refs/tags/$1"
 
-        # Directory Name if an asbsolute path
-        if [[ "${directoryName}" =~ ^/ ]]; then
-            targetDirectory="${directoryName}"
-        else
-            targetDirectory="${GIT_PROJECTS_DIRECTORY}/${directoryName}"
-        fi
+alias greset="git reset --hard"
 
-        # Else, it will be stored in the GIT_PROJECTS_DIRECTORY
-        if [[ ! -d "${targetDirectory}" ]]; then
-            Log ${INFO} "Cloning project into ${targetDirectory}"
-            gitClone "${targetDirectory}" "${remoteURL}"
-        elif [[ "${pullOn}" == "--pull" ]]; then
-            Log ${INFO} "Pulling existing git directory : ${targetDirectory}"
-            gitPull "${targetDirectory}"
-        else
-            Log ${INFO} "Fetching existing git directory : ${targetDirectory}"
-            gitFetch "${targetDirectory}"
-        fi
-    done
-}
+alias gc='git commit -v'
+alias 'gc!'='git commit -v --amend'
+alias gca='git commit -v -a'
+alias 'gca!'='git commit -v -a --amend'
+alias gcam='git commit -a -m'
